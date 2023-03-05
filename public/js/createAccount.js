dd99cc9b0157a135e9ffe4d4750df56fb7ce3187
Rad-Mad <75290770+Rad-Mad@users.noreply.github.com>
2023-03-05 03:30:02 -0600

    Add files via upload

--- public/js/createAccount.js
+++ public/js/createAccount.js
@@ -1,10 +1,46 @@
-import { createApp } from 'https://unpkg.com/petite-vue?module'
+const firebase = require('firebase');
+const { initializeApp } = require('firebase/app');
+require('firebase/database');
+
+const firebaseConfig = {
+    apiKey: "undefined",
+    authDomain: "undefined",
+    projectId: "login-sign-up-the-artmart",
+    storageBucket: "undefined",
+    messagingSenderId: "undefined",
+    appId: "1:472331438304:web:4cd9fba159c8888029a885"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = firebase.database();
 
 window.vue = createApp({
-    newPassword: '',
-    newPasswordConfirm: '',
-    createAccount() {
-        alert("create account logic goes here!");
-        window.location = '/';
+  newPassword: '',
+  newPasswordConfirm: '',
+  createAccount() {
+    const nameInput = document.querySelector('#nameInput');
+    const emailInput = document.querySelector('#emailInput');
+    const passwordInput = document.querySelector('#passwordInput');
+    const passwordConfirmInput = document.querySelector('#passwordConfirmInput');
+
+    const name = nameInput.value;
+    const email = emailInput.value;
+    const password = passwordInput.value;
+    const passwordConfirm = passwordConfirmInput.value;
+
+    if (password !== passwordConfirm) {
+      alert('Passwords do not match');
+      return;
     }
-}).mount()
+
+    firebase.database().ref('users/' + name).set({
+      name: name,
+      email: email,
+      password: password,
+    }).then(() => {
+      alert('User created successfully!');
+    }).catch((error) => {
+      alert('Error creating user: ' + error);
+    });
+  }
+}).mount();
--- public/js/login.js
+++ public/js/login.js
@@ -1,8 +1,37 @@
-import { createApp } from 'https://unpkg.com/petite-vue?module'
+// Import the necessary Firebase libraries
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
-window.vue = createApp({
-    handleLogin() {
-        alert("Login logic goes here!");
-        window.location = '/';
-    }
-}).mount()
+// Import petite-vue
+import { createApp } from 'https://unpkg.com/petite-vue?module';
+
+// Your Firebase project's configuration
+const firebaseConfig = {
+    apiKey: "undefined",
+    authDomain: "undefined",
+    projectId: "login-sign-up-the-artmart",
+    storageBucket: "undefined",
+    messagingSenderId: "undefined",
+    appId: "1:472331438304:web:4cd9fba159c8888029a885"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Create the Vue app
+const vm = createApp({
+  email: '',
+  password: '',
+  handleLogin() {
+    const auth = getAuth(app);
+    signInWithEmailAndPassword(auth, this.email, this.password)
+      .then(() => {
+        console.log('Successfully logged in.');
+        // Redirect the user to their dashboard or homepage
+      })
+      .catch((error) => {
+        console.error('Error logging in:', error.message);
+        // Display an error message to the user
+      });
+  }
+}).mount('#login');
