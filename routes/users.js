b95a4e0d3886c3797a274a183aa9786c8764c86a
Lys <alyssahammonds5453@gmail.com>
2023-03-06 18:04:40 -0600

    reworking createAccount

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-
-// This is a resource router. It doesn't serve pages, instead serving "resources".
-// For example, if you go to /users/4 it should return the user with ID 4 as an
-// object. This route doesn't exist yet though, right now it's just a placeholder.
+const firebaseManager = require("../modules/firebase.js");
 
 // GET users listing.
-router.get('/', function(req, res, next) {
+router.get("/", function (req, res, next) {
   // This is a placeholder
   // Normally it would send a user object or something
-  res.send('respond with a resource');
+  res.send("respond with a resource");
+});
+
+router.put("/register", (req, res, next) => {
+  console.log(req["body"]);
+  // firebaseManager.newUser(req.body["email"], req.body["password"]);
 });
 
 module.exports = router;
