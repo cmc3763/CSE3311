bea14c90719b371494d52960e7ac42281262aaa1
Benjamin J Farmer <benfarm17@gmail.com>
2023-03-31 17:01:36 -0500

    Forgot Password Functionality

--- public/js/forgot.js
+++ public/js/forgot.js
@@ -1,8 +1,37 @@
-import { createApp } from 'https://unpkg.com/petite-vue?module'
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
+import { getAuth,sendPasswordResetEmail} from "https://www.gstatic.com/firebasejs/9.17.2/firebase-auth.js";
+import {getFirestore} from "https://www.gstatic.com/firebasejs/9.17.2/firebase-firestore.js";
 
-window.vue = createApp({
-    sendPasswordResetEmail() {
-        alert("Password reset logic goes here");
-        window.location = '/';
-    }
-}).mount()
+const firebaseConfig = {
+    apiKey: "AIzaSyAVsw8cVFyZj5KwRAMID2Dc06amVD6_LVg",
+    authDomain: "login-for-the-artmart.firebaseapp.com",
+    databaseURL: "https://login-for-the-artmart-default-rtdb.firebaseio.com",
+    projectId: "login-for-the-artmart",
+    storageBucket: "login-for-the-artmart.appspot.com",
+    messagingSenderId: "453895598278",
+    appId: "1:453895598278:web:cac2395b4d9c635e54a7c7"
+  };
+
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+var email;
+
+
+const MyBtn = document.getElementById("TheButton");
+
+MyBtn.addEventListener("click",function(e){
+    email = document.getElementById("EmailBtn").value;
+    
+    sendPasswordResetEmail(auth,email)
+    .then(()=>{
+        alert("Email has been sent");
+        window.location.href = "http://localhost:3000/login";
+
+    })
+    .catch((error)=>{
+        alert(error);
+    });
+
+
+});
