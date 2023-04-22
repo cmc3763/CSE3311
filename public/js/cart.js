ca574f7ac3a2d6ebec10ac2bf70cfd5c4eec12e1
Lys <alyssahammonds5453@gmail.com>
2023-04-21 22:31:50 -0500

    cart fucntionality work

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,6 +1,24 @@
-
 // Protected page, if the user isn't logged in, redirect to /login
 if (!JSON.parse(sessionStorage.getItem("loggedIn"))) {
     window.location = "/login";
-  }
-  
+}
+    
+var removeCartItemButtons = document.getElementsByClassName('remove-button');
+var checkoutButton = document.getElementsByClassName('checkoutbutton');
+
+console.log(removeCartItemButtons)
+for (var i = 0; i < removeCartItemButtons.length; i++) {
+    var button = removeCartItemButtons[i]
+    button.addEventListener('click', function (event) {
+        var buttonClicked = event.target;
+        console.log("clicky clicky");
+        buttonClicked.parentElement.parentElement.remove();
+        updateCartTotal()
+    })
+}
+
+function updateCartTotal() {
+    //need to update cart total
+}
+
+//needs logic if there is nothing in the cart.
